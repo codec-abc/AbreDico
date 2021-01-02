43827004999ee710389447455695c02ccadab7d8
Budoc53 GG <viot.g.53@orange.fr>
2021-01-02 23:52:12 +0100

    Lecture de l'abre. Ajout de gifs.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,61 +29,145 @@ namespace AbreDico
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panelMIAP = new System.Windows.Forms.Panel();
+            this.labelMot = new System.Windows.Forms.Label();
+            this.ImageGai = new System.Windows.Forms.PictureBox();
+            this.ImageTriste = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.BtCreateTree = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.panelMIAP.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageGai)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageTriste)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMIAP
             // 
             this.panelMIAP.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
             this.panelMIAP.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelMIAP.Controls.Add(this.labelMot);
+            this.panelMIAP.Controls.Add(this.ImageGai);
+            this.panelMIAP.Controls.Add(this.ImageTriste);
+            this.panelMIAP.Controls.Add(this.textBox1);
             this.panelMIAP.Controls.Add(this.BtCreateTree);
-            this.panelMIAP.Location = new System.Drawing.Point(531, 30);
+            this.panelMIAP.Location = new System.Drawing.Point(530, 89);
             this.panelMIAP.Name = "panelMIAP";
-            this.panelMIAP.Size = new System.Drawing.Size(237, 135);
+            this.panelMIAP.Size = new System.Drawing.Size(421, 181);
             this.panelMIAP.TabIndex = 1;
+            // 
+            // labelMot
+            // 
+            this.labelMot.AutoSize = true;
+            this.labelMot.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMot.Location = new System.Drawing.Point(159, 33);
+            this.labelMot.Name = "labelMot";
+            this.labelMot.Size = new System.Drawing.Size(123, 25);
+            this.labelMot.TabIndex = 5;
+            this.labelMot.Text = "Mot à vérifier";
+            // 
+            // ImageGai
+            // 
+            this.ImageGai.Image = global::AbreDico.Properties.Resources.gai;
+            this.ImageGai.Location = new System.Drawing.Point(18, 0);
+            this.ImageGai.Name = "ImageGai";
+            this.ImageGai.Size = new System.Drawing.Size(52, 50);
+            this.ImageGai.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageGai.TabIndex = 4;
+            this.ImageGai.TabStop = false;
+            this.ImageGai.Visible = false;
+            // 
+            // ImageTriste
+            // 
+            this.ImageTriste.Image = global::AbreDico.Properties.Resources.triste;
+            this.ImageTriste.Location = new System.Drawing.Point(345, 5);
+            this.ImageTriste.Name = "ImageTriste";
+            this.ImageTriste.Size = new System.Drawing.Size(52, 50);
+            this.ImageTriste.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageTriste.TabIndex = 3;
+            this.ImageTriste.TabStop = false;
+            this.ImageTriste.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(18, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(379, 30);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
             // 
             // BtCreateTree
             // 
-            this.BtCreateTree.Location = new System.Drawing.Point(13, 19);
+            this.BtCreateTree.Location = new System.Drawing.Point(18, 114);
             this.BtCreateTree.Name = "BtCreateTree";
-            this.BtCreateTree.Size = new System.Drawing.Size(197, 41);
+            this.BtCreateTree.Size = new System.Drawing.Size(379, 41);
             this.BtCreateTree.TabIndex = 0;
-            this.BtCreateTree.Text = "Création de l\'arbre";
+            this.BtCreateTree.Text = "Vérification";
             this.BtCreateTree.UseVisualStyleBackColor = true;
-            this.BtCreateTree.Click += new System.EventHandler(this.BtCreateTree_Click);
+            this.BtCreateTree.Click += new System.EventHandler(this.BtVerifMot);
             // 
-            // listBox1
+            // pictureBox1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(30, 30);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(417, 372);
-            this.listBox1.TabIndex = 2;
+            this.pictureBox1.Image = global::AbreDico.Properties.Resources.tenor;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(999, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panelMIAP);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Vérification de l\'existance d\'un mot français";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.panelMIAP.ResumeLayout(false);
+            this.panelMIAP.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageGai)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageTriste)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panelMIAP;
         private System.Windows.Forms.Button BtCreateTree;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox ImageTriste;
+        private System.Windows.Forms.PictureBox ImageGai;
+        private System.Windows.Forms.Label labelMot;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace AbreDico
 {
-    /* Choix : l'abre est représenté par une liste de noeud
+    /* Choix : l'abre est représenté par une liste de noeuds
      * Le noeud est une classe qui comprend
      * - Un caractère : la donnée
      * - un booléen indiquant si cette lettre constitue la fin d'un mot
@@ -20,9 +20,14 @@ namespace AbreDico
 
 
     public partial class Form1 : Form
-    {    //=================================================================
-        // Variables globales
-        String chaine = "";
+    {
+        //=================================================================             
+        public Noeud NoeudRacine { get; private set; }
+        // crée une variable d'instance à laquelle on affectera le noeud racine (passage du pointeur) 
+        //pour en disposer dans Form1
+
+        private static readonly string MotResultatNom = "MOTSRESULTAT.txt";
+        
         //=================================================================
 
         public class Noeud
@@ -38,29 +43,35 @@ namespace AbreDico
             InitializeComponent();
         }
 
-
-
-        public void VerifAjoutLettre(Noeud NP, int rang, string Word)  //Création de l'arbre 
-        {   
-            if (Word.Length==0) { return; } // n'effectue pas le traitement pour un mot vide
-            char l;
-            l = Word[rang];                       
-            if (NP.DictionnaireDesSousNoeuds == null)
-            {   // si le dico n'existe pas on en crée un vierge
-                NP.DictionnaireDesSousNoeuds = new Dictionary<char, Noeud>();
-            }
-            if (NP.DictionnaireDesSousNoeuds.ContainsKey(l))//le dico existe et  si la clé existe
+        /// <summary>
+        /// Cette methode ajoute la lettre a l index courant au noeud Parent.
+        /// </summary>
+        /// <param name="noeudParent">Noeud auquel sera rajoute la lettre si elle n existe pas</param>
+        /// <param name="indexLettreCourante">Index de la lettre a rajouter si besoi de la string Word.</param>
+        /// <param name="Word">Mot de travail</param>
+        public void AjoutLettreCouranteSiBesoin(Noeud noeudParent, int indexLettreCourante, string Word)  //Création de l'arbre 
+        {
+            if (Word.Length == 0)
             {
-                foreach (KeyValuePair<char, Noeud> cle in NP.DictionnaireDesSousNoeuds)
+                return;
+            } // n'effectue pas le traitement pour un mot vide
+            char lettreCourante = Word[indexLettreCourante];
+            if (noeudParent.DictionnaireDesSousNoeuds == null)
+            {   // si le dico n'existe pas on en crée un vierge
+                noeudParent.DictionnaireDesSousNoeuds = new Dictionary<char, Noeud>();
+            }
+            if (noeudParent.DictionnaireDesSousNoeuds.ContainsKey(lettreCourante))//le dico existe et  si la clé existe
+            {
+                foreach (KeyValuePair<char, Noeud> cle in noeudParent.DictionnaireDesSousNoeuds)
                 {  // on cherche la clé (lettre)
-                    if (cle.Key == l) // clé identifiée 
+                    if (cle.Key == lettreCourante) // clé identifiée 
                     {
-                        rang++; //(pour lettre suivante du mot)
-                        if (rang < Word.Length ) //-1 viré
+                        indexLettreCourante++; //(pour lettre suivante du mot)
+                        if (indexLettreCourante < Word.Length) //-1 viré
                         { // si le traitement du mot n'est pas fini on appelle récursivement la procédure 
                           // en passant le noeud courant le rang incrémenté et le mot en paramètre.
-                           // MessageBox.Show("Le dico du noeud père contient " + l + "du mot " + Word + " On cherche " + Word[rang]);
-                            VerifAjoutLettre(cle.Value, rang, Word);
+                          // MessageBox.Show("Le dico du noeud père contient " + l + "du mot " + Word + " On cherche " + Word[rang]);
+                            AjoutLettreCouranteSiBesoin(cle.Value, indexLettreCourante, Word);
                         }
                     }
                 }
@@ -68,56 +79,75 @@ namespace AbreDico
             else
             {
                 //le dico existe et  clé pas trouvée => ajout noeud dans dico
-                Noeud NE = new Noeud();
-                NE.Lettre = l;
-                if (rang == Word.Length - 1)
+                Noeud noeudEnfant = new Noeud();
+                noeudEnfant.Lettre = lettreCourante;
+                if (indexLettreCourante == Word.Length - 1)
                 {   // dernière lettre du mot  : On ajoute le noeud correspondant
-                    NE.FinDeMot = true;
-                    NP.DictionnaireDesSousNoeuds.Add(l, NE);
-                  //  MessageBox.Show(l+" Dernière lettre du mot "+Word+" => sortie à l'arrache");
+                    noeudEnfant.FinDeMot = true;
+                    noeudParent.DictionnaireDesSousNoeuds.Add(lettreCourante, noeudEnfant);
+                    //  MessageBox.Show(l+" Dernière lettre du mot "+Word+" => sortie à l'arrache");
                     return;
                 }
                 else
                 {   // PAS dernière lettre du mot  : On ajoute le noeud correspondant et on incémente rang et on relance récursivement la procédure
-                    NE.FinDeMot = false;
-                    NP.DictionnaireDesSousNoeuds.Add(l, NE);
-                   // MessageBox.Show(l + " Pas dernière lettre du mot " + Word + " => ajout noeud");
-                    rang++;
-                    VerifAjoutLettre(NE, rang, Word);
+                    noeudEnfant.FinDeMot = false;
+                    noeudParent.DictionnaireDesSousNoeuds.Add(lettreCourante, noeudEnfant);
+                    // MessageBox.Show(l + " Pas dernière lettre du mot " + Word + " => ajout noeud");
+                    indexLettreCourante++;
+                    AjoutLettreCouranteSiBesoin(noeudEnfant, indexLettreCourante, Word);
                 }
             }
         }
 
-        private void DecodeArbre(Noeud NP) // Lecture des mots
-        {          
-            if (NP.DictionnaireDesSousNoeuds != null)
-            {   // dictionnaire du noeud existe              
-                foreach (KeyValuePair<char, Noeud> cle in NP.DictionnaireDesSousNoeuds)
-                {
-                   // MessageBox.Show(NP.Lettre + " Valeur Dico " + cle.Key);
-                    if (cle.Value.DictionnaireDesSousNoeuds == null)
-                    { // n'a pas d'enfant                                
-                        chaine = chaine + cle.Value.Lettre;
-                        this.listBox1.Items.Add(chaine);
-                        chaine = "";
-                    }
-                    else
-                    { // a des enfants                    
-                        chaine = chaine + cle.Value.Lettre;
-                        if (cle.Value.FinDeMot) { this.listBox1.Items.Add(chaine); }
-                        DecodeArbre(cle.Value);
-                    }                  
-                }
-            }
-        }
+        //private void DecodeArbre(Noeud NP) // Lecture des mots
+        //{
+        //    StreamWriter sw2 = new StreamWriter(MotResultatNom);
+        //    try
+        //    {
+        //        if (NP.DictionnaireDesSousNoeuds != null)
+        //        {   // dictionnaire du noeud existe              
+        //            foreach (KeyValuePair<char, Noeud> cle in NP.DictionnaireDesSousNoeuds)
+        //            {
+        //                if (cle.Value.DictionnaireDesSousNoeuds == null)
+        //                { // n'a pas d'enfant             
+        //                    sw2.WriteLine(cle.Value.Lettre);
+        //                    //  this.listBox1.Items.Add(cle.Value.Lettre+".");                   
+        //                }
+        //                else
+        //                { // a des enfants                                          
+        //                    if (cle.Value.FinDeMot)
+        //                    {
+        //                        sw2.WriteLine(cle.Value.Lettre);
+        //                        //  this.listBox1.Items.Add(cle.Value.Lettre + "=>");
+        //                    }
+        //                    else
+        //                    {
+        //                        sw2.WriteLine(cle.Value.Lettre);
+        //                        // this.listBox1.Items.Add(cle.Value.Lettre + "=> ...");
+        //                    }
+        //                    DecodeArbre(cle.Value);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        throw;
+        //    }
+        //    finally
+        //    {
+        //        sw2.Close();
+        //    }
+        //}
 
-        private void BtCreateTree_Click(object sender, EventArgs e)
+        private void Init()  // initialisation des données
         {
             string mot;
+            this.pictureBox1.Visible = true;
             // Création de l'arbre à partir du fichier texte.
-            //========================
+            //===================
             // initialisation du dictionnaire
-            string NomDuDico = "H:\\Famille\\GERALD\\visual_Studio\\Arbre_Dico\\fichierTest.txt";
+            string NomDuDico = "H:\\Famille\\GERALD\\visual_Studio\\Arbre_Dico\\MOTS TRADUITS.txt";
             if (!File.Exists(NomDuDico))
             {
                 MessageBox.Show(NomDuDico + " n'existe pas");
@@ -132,18 +162,112 @@ namespace AbreDico
                 Racine.Lettre = ' ';
                 Racine.FinDeMot = false;
                 Racine.DictionnaireDesSousNoeuds = null;
-                // traitement des mots
+                // traitement des mots               
                 for (int i = 0; i <= lignesDico.Length - 1; i++)  // traitement des lettres du mot
                 {
                     mot = lignesDico[i];
-                    // MessageBox.Show(mot);
+                    //  this.listBox2.Items.Add(mot);
+                    //  MessageBox.Show(mot);
                     // Création de la branche correspondant au mot par passage du noeud racine à la prcédure récussive VerifAjouteLettre                     
-                    VerifAjoutLettre(Racine, 0, mot);
+                    AjoutLettreCouranteSiBesoin(Racine, 0, mot);
                 }
-                // lecture de l'arbre
-                this.listBox1.Items.Clear();
-                DecodeArbre(Racine);
+                // lecture de l'arbre                             
+                //DecodeArbre(Racine);
+                this.pictureBox1.Left += 50;
+                this.NoeudRacine = Racine; // affecte à NoeudRacine accessible partout dans form1 la valeur du pointeur de Racine
+                this.pictureBox1.Visible = false;
             }
+        }
+
+        private void BtVerifMot(object sender, EventArgs e)
+        {
+            VerifMot();
+        }
+
+        private void VerifMot()
+        {
+            timer1.Stop();
+            this.pictureBox1.Visible = false;
+            string msg;
+            if (VerifMot(this.textBox1.Text.ToUpper()))
+            {
+                this.ImageGai.Visible = true;
+                msg = "Le mot existe";
+            }
+            else
+            {
+                this.ImageTriste.Visible = true;
+                msg = "Le mot n'existe PAS";
+            }
+            MessageBox.Show(msg);
+        }
+
+        public Noeud Nt2;
+
+        bool VerifMot(string Mot)
+        {
+            int lg = Mot.Length - 1;
+            Noeud Nt = NoeudRacine;
+            for (int i = 0; i <= lg; i++) // Faire pour toutes les lettres du mot
+            {
+                char c = Mot[i];
+                if (Nt.DictionnaireDesSousNoeuds != null) // le Dictionnaire du noeud examiné n'est pas null
+                {
+                    if (Nt.DictionnaireDesSousNoeuds.ContainsKey(c))// contient la lettre du mot
+                    {
+                        foreach (KeyValuePair<char, Noeud> cle in Nt.DictionnaireDesSousNoeuds)
+                        {
+                            if (cle.Key == c) // clé identifièe (pour noeud suivant)
+                            {
+                                Nt2 = cle.Value;
+                            }
+                        }
+                        Nt = Nt2; // affectation du noeud trouvé pour le tour de boucle suivant                           
+                    }
+                    else
+                    {  // la lettre n'est pas trouvée !
+                        Nt2 = null;
+                        return false;
+                    }
+                }
+                else
+                { //le dictionnaire est null
+                    if (i != lg + 1)
+                    { 
+                        return false; 
+                    } // si ce n'est pas la fin de mot c'est anormal on retourne false
+                    else
+                    {
+                        return true;
+                    }// si fin de mot c'est normal on retourne true
+                }
+            }
+            // pour satisfaire le compilo
+            return true;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            this.pictureBox1.Visible = true;
+            this.ImageGai.Visible = false;
+            this.ImageTriste.Visible = false;
+        }
+
+        int t = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+             t++;
+            textBox1.Text = t.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
         }
     }
 }
